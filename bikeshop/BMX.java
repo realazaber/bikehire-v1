40da8195446e8e6103eb40bdf98a1876b78f76db
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-05-23 20:55:40 +1000

    Code Upload

---
+++ bikeshop/BMX.java
@@ -0,0 +1,26 @@
+// Subclass of Bike 
+
+package bikeshop;
+
+public class BMX extends Bike{
+	private String _make = "BMX";
+	private double _pricePerDay = 40.00;
+
+	
+	public BMX(boolean _isRented, String _name){
+		super(_isRented);
+		this._name = _name;
+	}
+	
+	public String getType(){
+		return _make;
+	}
+	
+	public double getPrice(){
+		return _pricePerDay;
+	}
+	
+	public String toString(){
+		return super.toString() + "\nType: " + _make + "\nPrice per day: "+ _pricePerDay;
+	}
+}
---
+++ bikeshop/Main.java
@@ -0,0 +1,8 @@
+package bikeshop;
+
+public class Main {
+	public static void main(String[] args){
+		BikeHire _shop = new BikeHire();
+		_shop.run();
+	}
+}
